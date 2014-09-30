660a0d373533265fd038b4b0e953e33360939f77
Nicholas Cole <nicholasjcole1@gmail.com>
2014-09-30 08:59:12 -0500

    Added db size management

--- get_app_idea_tweets.py
+++ get_app_idea_tweets.py
@@ -21,6 +21,21 @@ cursor = db.cursor()
 #cursor.execute('''CREATE TABLE tweets(id TEXT, profname TEXT, screenname TEXT, profimagelink TEXT, tweetcontent TEXT, tweetdate TEXT)''')
 
 
+# make db smaller if more than N rows
+
+db_size = cursor.execute("select count(*) from tweets").fetchall()[0][0]
+if db_size > 500:
+    cursor.execute("delete from tweets where rowid in (Select rowid from tweets limit 50)")
+
+db.commit()
+    
+    
+
+
+# good way to get field names if forgotten
+#cursor.execute("PRAGMA table_info(tweets);")
+
+
 
 
 
@@ -44,24 +59,27 @@ results = search_tweets('"There should be an app"')
 # ['screen_name']
 # ['profile_image_url']
 
-for status in results['statuses']:
+for status in reversed(results['statuses']):
     # get rid of RTs
     if "RT" not in status['text']:
-        tweet_id = status['id']
-        tweet_content = status['text']
-        tweet_prof_image = status['user']['profile_image_url']
-        tweet_prof_name = status['user']['screen_name']
-        tweet_actual_name = status['user']['name']
-        tweet_create_time = status['created_at']
-        #print tweet_content
-        cursor.execute('''INSERT INTO tweets(id, profname, screenname, profimagelink, tweetcontent, tweetdate) 
+        # if tweet_id not already in databse:
+        if len(cursor.execute("select id from tweets where tweetcontent=?", (status['text'],)).fetchall()) == 0:
+            tweet_id = status['id']
+            tweet_content = status['text']
+            tweet_prof_image = status['user']['profile_image_url']
+            tweet_prof_name = status['user']['screen_name']
+            tweet_actual_name = status['user']['name']
+            tweet_create_time = status['created_at']
+            print tweet_create_time
+            # need to check if tweet_id in db, if so then we shouldn't add to db
+            cursor.execute('''INSERT INTO tweets(id, profname, screenname, profimagelink, tweetcontent, tweetdate) 
             VALUES (?,?,?,?,?,?)''', (tweet_id, tweet_prof_name, tweet_actual_name, tweet_prof_image, tweet_content, tweet_create_time))
-    # add to db
-    # db should contain:
-    # tweet id, tweet profile name, tweet prof image link, tweet content, tweet
-
-
+    print(cursor.execute("select count(*) from tweets").fetchall())
 db.commit()    
+
+# get last ten entries
+# cursor.execute("select * from tweets order by tweetdate desc limit 10")
+
 
 #~ for status in ai.results['statuses']:
     #~ # get rid of RTs
